187bdf6f1168a54646f3f0bb1834f17dac41c860
osama <osama.salman99@gmail.com>
2021-05-20 00:36:36 +0300

    update

--- src/1a.py
+++ src/1a.py
@@ -2,5 +2,9 @@ from helper import *
 
 print(get_language('en'))
 data = get_pageview_data(r'..\res\pageviews-20210401-120000.txt')
+i = 0
 for datum in data:
 	print(datum)
+	if i > 500:
+		break
+	i += 1
--- src/helper.py
+++ src/helper.py
@@ -23,35 +23,76 @@ def get_pageview_data(file_path):
 	file = open(file_path, encoding='utf8')
 	pageviews = []
 	for line in file.readlines():
-		pageviews.append(line.strip().split(' ')[0:3])
+		line = line.strip().split(' ')
+		pageviews.append(PageView(line[0], line[1], line[2]))
 	return pageviews
 
 
 class PageView:
-	pattern = re.compile(r'[a-z]+\.(b|d|m|mw|n|q|s|v|w)$')
-	projects = {'b': 'wikibooks', 'd': 'wiktionary', 'm': 'wikimedia', 'mw': 'wikipedia mobile', 'n': 'wikinews',
-				'q': 'wikiquote', 's': 'wikisource', 'v': 'wikiversity', 'w': 'mediawiki'}
+	pattern = re.compile(r'[a-z]+(\.(b|d|m|mw|n|q|s|v|w))+$')
+	projectsDict = {'b': 'wikibooks', 'd': 'wiktionary', 'm': 'wikimedia', 'mw': 'wikipedia mobile', 'n': 'wikinews',
+					'q': 'wikiquote', 's': 'wikisource', 'v': 'wikiversity', 'w': 'mediawiki'}
 
 	def __init__(self, language_code, page_title, number_of_views):
 		language = language_code
-		project = 'wikipedia'
-		if PageView.pattern.match(language_code):
+		projects = ['wikipedia']
+		match = PageView.pattern.match(language_code)
+		if match:
 			language_code = language_code.split('.')
 			language = language_code[0]
-			project = language_code[1]
+			projects = []
+			for project in language_code[1:]:
+				projects.append(PageView.projectsDict[project])
+		try:
+			language = get_language(language)
+		except KeyError:
+			pass
 		self.language = language
-		self.project = project
+		self.language = language
+		self.projects = projects
 		self.page_title = page_title
-		self.number_of_views = number_of_views
+		self.number_of_views = int(number_of_views)
 
 	def get_language(self):
 		return self.language
 
-	def get_project(self):
-		return self.project
+	def get_projects(self):
+		return self.projects
 
 	def get_page_title(self):
 		return self.page_title
 
 	def get_number_of_views(self):
 		return self.get_number_of_views()
+
+	def __str__(self):
+		return self.language + '\t' + str(self.projects) + '\t' + self.page_title + '\t' + str(self.number_of_views)
+
+
+class GeoEdit:
+	def __init__(self, language, country, activity, lower_bound, upper_bound):
+		language = language.replace('wiki', '')
+		try:
+			language = get_language(language)
+		except KeyError:
+			pass
+		self.language = language
+		self.country = country
+		self.activity = activity
+		self.lowerBound = int(lower_bound)
+		self.upperBound = int(upper_bound)
+
+	def get_language(self):
+		return self.language
+
+	def get_country(self):
+		return self.country
+
+	def get_activity(self):
+		return self.activity
+
+	def get_lower_bound(self):
+		return self.lowerBound
+
+	def get_upper_bound(self):
+		return self.upperBound
