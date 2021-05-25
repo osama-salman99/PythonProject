7c527caf6ee7a9f428f83a99efe68c88e39ce338
osama <osama.salman99@gmail.com>
2021-05-26 01:18:04 +0300

    update

--- src/by_country.py
+++ src/by_country.py
@@ -5,22 +5,23 @@ import pandas as pd
 
 dataframe = get_geoeditors_data(r'../res/geoeditors-monthly-2021-03.tsv')
 countries = dataframe['Country'].unique()
-data = pd.DataFrame(columns=['Country', 'ISOs', 'Value'])
+data = pd.DataFrame(columns=['Country', 'ISOs', 'Editors'])
 for country in countries:
 	iso = country
 	try:
 		countryData = pycountry.countries.search_fuzzy(country)
 		iso = countryData[0].alpha_3
 	except LookupError:
+		print(f'Could not find ISO for {country}')
 		pass
 	row = {'Country': country,
 		   'ISOs': iso,
-		   'Value': sum(dataframe[dataframe['Country'] == country]['Upper Bound'])}
+		   'Editors': sum(dataframe[dataframe['Country'] == country]['Upper Bound'])}
 	data = data.append(row, ignore_index=True)
-data['Value'] = data['Value'].astype(int)
+data['Editors'] = data['Editors'].astype(int)
 fig = px.choropleth(data_frame=data,
 					locations='ISOs',
-					color='Value',
+					color='Editors',
 					hover_name='Country',
 					color_continuous_scale='RdYlGn')
 fig.show()
